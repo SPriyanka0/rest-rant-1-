451ba05d1db54c07b210a553fc6ce99e1fb291cd
Priyanka Shah <priyanka2003shah@gmail.com>
2022-05-18 20:37:30 -0400

    adding now

--- index.js
+++ index.js
@@ -25,6 +25,7 @@
 //    d. Call res.send(‘hello world’) 
 
 // 11. Call app.listen(3000) to keep server open
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
@@ -32,4 +33,9 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
+
